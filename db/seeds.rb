4b3af7f368e6a36a3144bf4a4f19b605d07fdac7
Leslie <Kooliesk@gmail.com>
2023-08-13 12:06:08 +0200

    create messages seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+greetings = [
+    'Good morning!',
+    'Hello!',
+    'Hi there!',
+    'Greetings!',
+    'Welcome!'
+  ]
+  
+  greetings.each do |greeting|
+    Message.create(greeting: greeting)
+  end
